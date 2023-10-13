6143b548b6a326ddaefdf9e5de7193650a0dba64
Aref Keshavarz <arefkeshavarz1220@gmail.com>
2023-10-13 13:39:51 +0330

    add functions and all of thing that need program and finish program . * finish .

--- main.py
+++ main.py
@@ -1,13 +1,67 @@
 import random
 
 print("Hello user , welcome to Hangman game :) ")
-words = ('ant baboon badger bat bear beaver camel cat clam cobra cougar '
-         'coyote crow deer dog donkey duck eagle ferret fox frog goat '
-         'goose hawk lion lizard llama mole monkey moose mouse mule newt '
-         'otter owl panda parrot pigeon python rabbit ram rat raven '
-         'rhino salmon seal shark sheep skunk sloth snake spider '
-         'stork swan tiger toad trout turkey turtle weasel whale wolf '
+# step 0 : words and pictures :
+words = ('ant badger bat bear camel cat clam cobra cougar '
+         'crow dog donkey duck fox frog goat '
+         'hawk lion lizard mole monkey mouse mule newt '
+         'owl pigeon python ram rat raven '
+         'rhino salmon seal shark skunk sloth snake spider '
+         'stork swan tiger toad trout turkey whale wolf '
          'wombat zebra ').split()
+
+HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+number_of_wrong_answer = 0
+
 # step 1 : choise a word :
 word = random.choice(words)
 
@@ -15,9 +69,43 @@ word = random.choice(words)
 # step 2 : generate a place for letters :
 def build_place(word):
     places = ""
-    for i in range(0, len(word)):
+    for i in word:
         places += "_"
     return places
 
 
 places = build_place(word)
+
+# step 3 : ask letter from user and checking :
+print(places)
+
+
+def check_letter(letter, word, places, numebr_of_wrong_answer):
+    if (letter in word):
+        index_of_letter = word.index(letter)
+        places = list(places)
+        places[index_of_letter] = letter
+        places = "".join(places)
+        if (not ('_' in places)):
+            print(places)
+            print("----------------------------")
+            print(">>> you win ... ")
+            exit()
+        else:
+            return (places, numebr_of_wrong_answer)
+    else:
+        numebr_of_wrong_answer += 1
+        print(HANGMANPICS[numebr_of_wrong_answer])
+        if (numebr_of_wrong_answer == 6):
+            print("------------------------------")
+            print(">>> you lost ... ")
+            exit()
+        return (places, numebr_of_wrong_answer)
+
+
+while number_of_wrong_answer <= 6:
+    letter = input("please guess letter : ")
+    places, number_of_wrong_answer = check_letter(letter, word, places, number_of_wrong_answer)
+    print(f"{places}")
+
+#-------------------------  finish  -------------------------
